0d1d8f42f2dfbc1798e2f1073385d7d98c1b2e24
vijay gupta <gupta.vijay@gmail.com>
2016-12-18 20:35:50 -0800

    upgrade to 5

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This install file pulls & installs elasticsearch and kibana
 
+ES=/usr/share/elasticsearch
 VOL=$HOME/PERSIST
 
 mkdir -p $VOL
@@ -26,7 +27,7 @@ docker rm elasticsearch
 docker rm kibana
 docker rm packetbeat
 
-docker run -d -p 9200:9200 -p 9300:9300 --name elasticsearch -v "$VOL/elasticsearch/config":/usr/share/elasticsearch/config -v "$VOL/elasticsearch/data":/usr/share/elasticsearch/data elasticsearch:5
+docker run -d -p 9200:9200 -p 9300:9300 --name elasticsearch -v "$VOL/elasticsearch/config":$ES/config -v "$VOL/elasticsearch/data":$ES/data elasticsearch:5
 docker run -d -p 5601:5601 --name kibana --link elasticsearch:elasticsearch -e ELASTICSEARCH_URL=http://elasticsearch:9200 kibana:5
 docker run -d --network=host --name packetbeat -v "$VOL/packetbeat":/etc/packetbeat ghoranyi/docker-agent-pb
 
