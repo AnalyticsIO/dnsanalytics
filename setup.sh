016b7b9027b1d0dbea7aa8ec2de0199ee647949b
vijay gupta <gupta.vijay@gmail.com>
2016-12-21 16:08:41 -0800

    updated for packetbeat 5.1

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ VOL=$HOME/PERSIST
 
 mkdir -p $VOL
 
-cp -vr setup-res/elasticsearch $VOL
-cp -vr setup-res/kibana $VOL
-cp -vr setup-res/packetbeat $VOL
+cp -vr resource/elasticsearch $VOL
+cp -vr resource/kibana $VOL
+cp -vr resource/packetbeat $VOL
 
 OUT=$?
 if [ $OUT -ne 0 ];then
@@ -17,7 +17,7 @@ fi
 
 docker pull elasticsearch:5
 docker pull kibana:5
-docker pull ghoranyi/docker-agent-pb
+docker pull gsvijay/packetbeat
 
 docker stop kibana
 docker stop packetbeat
@@ -29,5 +29,5 @@ docker rm packetbeat
 
 docker run -d -p 9200:9200 -p 9300:9300 --name elasticsearch -v "$VOL/elasticsearch/config":$ES/config -v "$VOL/elasticsearch/data":$ES/data elasticsearch:5
 docker run -d -p 5601:5601 --name kibana --link elasticsearch:elasticsearch -e ELASTICSEARCH_URL=http://elasticsearch:9200 kibana:5
-docker run -d --network=host --name packetbeat -v "$VOL/packetbeat":/etc/packetbeat ghoranyi/docker-agent-pb
+docker run -d --network=host --name packetbeat -v "$VOL/packetbeat":/etc/packetbeat gsvijay/packetbeat
 
